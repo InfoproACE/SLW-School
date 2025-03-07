0fd0c0baf01188bec15a3ee3173af88563b7cb3f
InfoproACE <infopro2570@gmail.com>
2025-03-07 08:32:59 +0700

    Update teacher.js

--- programs/js/teacher.js
+++ programs/js/teacher.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const user = JSON.parse(sessionStorage.getItem("loggedInUser"));
     if (!user) {
-        window.location.href = "https://script.google.com/macros/s/AKfycbwy0lJqri9OKOxQgOCgzXT-Htjyml0J0hSAVkvQtN_Aw2ndNshX8ZxSj7rcHeTMDUSn/exec";  // หากไม่มีผู้ใช้ล็อกอินอยู่ ให้กลับไปหน้า login
+        window.location.href = "login";  // หากไม่มีผู้ใช้ล็อกอินอยู่ ให้กลับไปหน้า login
     } else {
         document.getElementById("userProfile").textContent = `${user.firstName} ${user.lastName}`;
         document.getElementById("profilePic").src = user.profilePic;
